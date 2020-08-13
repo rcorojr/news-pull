c54a96ada8fcb8acf534852e34884efde7338f4b
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-08-13 12:47:29 -0600

    post test: removed heroku connector - fixed comment and article match

--- server.js
+++ server.js
@@ -16,35 +16,28 @@ var app = express();
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 // Connect to Heroku
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
 
 // Routes
 
-// A GET route for scraping the echoJS website
+// A GET route for scraping the website
 app.get("/scrape", function(req, res) {
-    // First, we grab the body of the html with axios
     axios.get("https://old.reddit.com/r/webdev").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
-      // Now, we grab every h2 within an article tag, and do the following:
       $("p.title").each(function(i, element) {
-        // Save an empty result object
         var result = {};
   
-        // Add the text and href of every link, and save them as properties of the result object
         result.title = $(this)
           .children("a")
           .text();
@@ -52,19 +45,15 @@ app.get("/scrape", function(req, res) {
           .children("a")
           .attr("href");
   
-        // Create a new Article using the `result` object built from scraping
         db.Article.create(result)
           .then(function(dbArticle) {
-            // View the added result in the console
             console.log(dbArticle);
           })
           .catch(function(err) {
-            // If an error occurred, log it
             console.log(err);
           });
       });
   
-      // Send a message to the client
       res.send("Scrape Complete");
     });
   });
