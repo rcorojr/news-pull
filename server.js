04c3f719b0d82d0675da2fd9acefedf648dbedc9
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-08-13 23:59:26 -0600

    fixed NYTimes pull and push to html

--- server.js
+++ server.js
@@ -21,41 +21,45 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-// mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 // Connect to mongo Heroku
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongonews-pull";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongonews-pull";
 
-mongoose.connect(MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
 
 // Routes
 
 // A GET route for scraping the website
 app.get("/scrape", function(req, res) {
-    axios.get("https://old.reddit.com/r/webdev/").then(function(response) {
+  db.Article.remove({}).then(function(response){
+    axios.get("https://www.nytimes.com/").then(function(response) {
       var $ = cheerio.load(response.data);
   
-      $("p.title").each(function(i, element) {
+      $("div.css-6p6lnl").each(function(i, element) {
         var result = {};
-  
+            // console.log($(this).find("a").attr("href"))
         result.title = $(this)
-          .children("a")
+          .find("h2")
           .text();
-        result.link = $(this)
-          .children("a")
+        result.link = "https://www.nytimes.com/" + $(this)
+          .find("a")
           .attr("href");
   
         db.Article.create(result)
           .then(function(dbArticle) {
-            console.log(dbArticle);
+            console.log(dbArticle); res.send("Scrape Complete");
           })
           .catch(function(err) {
             console.log(err);
           });
       });
-  
-      res.send("Scrape Complete");
+      console.log("test");
+
+      
     });
+  });
+    
   });
 
   // Route for getting all Articles from the db
